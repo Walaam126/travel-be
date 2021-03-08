f44ab3a49cb22cc4e0dba6a540895f15e4121c11
HawraAmeer <hwra.ameer97@gmail.com>
2021-03-08 16:14:41 +0300

    Sign up Completed

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const db = require("./db/models");
 const express = require("express");
 const cors = require("cors");
+
+// Data-base
+const db = require("./db/models");
+
+// Authentication
 const passport = require("passport");
 const { localStrategy } = require("./middleware/passport");
+
+// Routes
 const userRoutes = require("./routes/users");
+
 const app = express();
 
 app.use(cors());
@@ -13,12 +20,14 @@ passport.use(localStrategy);
 
 app.use(userRoutes);
 
-app.use((req, res, next) => {
-  next({
-    status: 404,
-    message: "Path not found",
-  });
-});
+app.use((req, res, next) => next({ status: 404, message: "Path not found" }));
+
+app.use((err, req, res, next) =>
+  res
+    .status(err.status ?? 500)
+    .json({ message: err.message ?? "Internal Server Error!" })
+);
+
 db.sequelize.sync({ alter: true });
 
 app.listen(8000, () => {
--- db/models/User.js
+++ db/models/User.js
@@ -2,6 +2,10 @@ module.exports = (sequelize, DataTypes) =>
   sequelize.define("User", {
     username: { type: DataTypes.STRING, allowNull: false, unique: true },
     password: { type: DataTypes.STRING, allowNull: false },
-    email: { type: DataTypes.STRING, validate: { isEmail: true } },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: { isEmail: true },
+    },
     isAirline: { type: DataTypes.BOOLEAN, defaultValue: false },
   });
