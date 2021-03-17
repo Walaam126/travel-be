05af663ff410ca0de8ba90ed96ad97a3872295f5
HawraAmeer <hwra.ameer97@gmail.com>
2021-03-17 14:03:09 +0300

    Flight Codes Done

--- db/models/Flight.js
+++ db/models/Flight.js
@@ -6,8 +6,9 @@ module.exports = (sequelize, DataTypes) =>
       depTime: { type: DataTypes.STRING, allowNull: false },
       arrDate: { type: DataTypes.DATEONLY, allowNull: false },
       arrTime: { type: DataTypes.STRING, allowNull: false },
-      price: { type: DataTypes.INTEGER, allowNull: false },
+      ePrice: { type: DataTypes.INTEGER, allowNull: false },
       economy: { type: DataTypes.INTEGER, allowNull: false },
+      bPrice: { type: DataTypes.INTEGER, allowNull: false },
       business: { type: DataTypes.INTEGER, allowNull: false },
     },
     { timestamps: false }
